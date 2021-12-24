304668b7ce982887b79dd0b0fb5b0140fdabe6eb
sergnio <sergnioa@gmail.com>
2021-12-23 22:21:36 -0600

    checkpoint

--- cypress/integration/1-getting-started/playgame.spec.js
+++ cypress/integration/1-getting-started/playgame.spec.js
@@ -11,9 +11,10 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe("example to-do app", () => {
-  let abort = false;
+const checkForRoute = () => {};
+const filePath = "cypress/fixtures/shouldContinueFindingRoutes.txt";
 
+describe("example to-do app", () => {
   beforeEach(() => {
     cy.login();
   });
@@ -30,182 +31,69 @@ describe("example to-do app", () => {
     cy.wait(500);
     // this can be dynamic
     cy.get("span#airport_52").click();
-    cy.wait(1000);
-
-    // cy.get("div#aircraftList").within(() => {
-    //   cy.waitUntil(
-    //     () => cy.get("div.loadingWheel").should("have.class", "hidden"),
-    //     { timeout: 2000 }
-    //   );
-    // });
+    cy.wait(500);
 
     // Look in the box that has all aircrafts
     cy.get(".aircraftsBox").each(($el, index, $list) => {
       // within that, find the "boxes" which have 6 aircrafts
-      cy.get("div.aircraftListMiniBox").each(($element) => {
-        const matchesZeroPercent = cy.wrap($element).contains("0%");
+
+      // loop through each of the planes (6 times)
+      cy.get("div.aircraftListMiniBox").each(($plane) => {
+        cy.writeFile(filePath, "false");
+        // todo - this doesn't work...
+        const matchesZeroPercent = cy.wrap($plane).contains(": 0%");
 
         if (matchesZeroPercent) {
-          cy.wrap($element).click();
+          cy.wrap($plane).click();
 
-          // now find the routes which have 1190 passengers
+          // now find the routes which have 1195 passengers
           // ** iterate over the routes **
+          // loop thru each route
           cy.get("span.lineList").each(($line) => {
-            cy.log("should abort: " + abort);
-            if (abort) {
-              cy.log("aborting");
-              cy.wait(2000);
-              return false;
-            }
-
-            cy.wrap($line).click();
-
-            const returnVal = cy.get("tr#demandDay0 td.greenBonus").invoke(
-              "text",
-              (index, theText) => {
-                // text is formatted like "1190 PAX"
-                const paxValue = theText.split(" ")[0];
-
-                if (paxValue >= 1195) {
-                  cy.get("td#caseIndex0").click();
-                  cy.log("Great success", paxValue);
-                  
-                  cy.get("img.planningDuplicate").first().click();
-                  abort = true;
-                  cy.wait(2000);
-                  return false;
-                } else {
-                  cy.log("not enough pax", paxValue);
-                }
+            cy.readFile(filePath).then((found1195) => {
+              cy.log("use1195 value:", found1195);
+              // if we've found it, just break it out
+              if (found1195 === "true") {
+                cy.log("found this to be true");
+                cy.writeFile(filePath, "false");
+                cy.wait(40000);
+                return false;
               }
-            );
-            
+
+              cy.wrap($line).click();
+
+              const routePAX = "tr#demandDay0 td.greenBonus";
+              cy.get(routePAX)
+                .first()
+                .invoke("text", (_, theText) => {
+                  // text is formatted like "1190 PAX"
+                  const paxValue = theText.split(" ")[0];
+
+                  if (paxValue >= 1195) {
+                    // selects the routes for all days and saves it
+                    cy.get("td#caseIndex0").click();
+                    cy.log("Great success", paxValue);
+                    cy.get("img.planningDuplicate")
+                      .first()
+                      .click()
+                      .then(() => {
+                        cy.log("setting this to true");
+                        cy.writeFile(filePath, "true");
+                        cy.wait(40000);
+                      });
+                    return false;
+                  } else {
+                    cy.log("not enough pax", paxValue).then(() => {
+                      cy.log("setting this to false");
+                    });
+                  }
+                });
+            });
           });
         } else {
           cy.log("doesnt match");
         }
       });
-    });
-  });
-
-  xit("displays two todo items by default", () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get(".todo-list li").should("have.length", 2);
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
-    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
-  });
-
-  xit("can add new todo items", () => {
-    // We'll store our item text in a variable so we can reuse it
-    const newItem = "Feed the cat";
-
-    // Let's get the input element and use the `type` command to
-    // input our new list item. After typing the content of our item,
-    // we need to type the enter key as well in order to submit the input.
-    // This input has a data-test attribute so we'll use that to select the
-    // element in accordance with best practices:
-    // https://on.cypress.io/selecting-elements
-    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
-
-    // Now that we've typed our new item, let's check that it actually was added to the list.
-    // Since it's the newest item, it should exist as the last element in the list.
-    // In addition, with the two default items, we should have a total of 3 elements in the list.
-    // Since assertions yield the element that was asserted on,
-    // we can chain both of these assertions together into a single statement.
-    cy.get(".todo-list li")
-      .should("have.length", 3)
-      .last()
-      .should("have.text", newItem);
-  });
-
-  xit("can check off an item as completed", () => {
-    // In addition to using the `get` command to get an element by selector,
-    // we can also use the `contains` command to get an element by its contents.
-    // However, this will yield the <label>, which is lowest-level element that contains the text.
-    // In order to check the item, we'll find the <input> element for this <label>
-    // by traversing up the dom to the parent element. From there, we can `find`
-    // the child checkbox <input> element and use the `check` command to check it.
-    cy.contains("Pay electric bill")
-      .parent()
-      .find("input[type=checkbox]")
-      .check();
-
-    // Now that we've checked the button, we can go ahead and make sure
-    // that the list element is now marked as completed.
-    // Again we'll use `contains` to find the <label> element and then use the `parents` command
-    // to traverse multiple levels up the dom until we find the corresponding <li> element.
-    // Once we get that element, we can assert that it has the completed class.
-    cy.contains("Pay electric bill")
-      .parents("li")
-      .should("have.class", "completed");
-  });
-
-  context("with a checked task", () => {
-    beforeEach(() => {
-      // We'll take the command we used above to check off an element
-      // Since we want to perform multiple tests that start with checking
-      // one element, we put it in the beforeEach hook
-      // so that it runs at the start of every test.
-      cy.contains("Pay electric bill")
-        .parent()
-        .find("input[type=checkbox]")
-        .check();
-    });
-
-    xit("can filter for uncompleted tasks", () => {
-      // We'll click on the "active" button in order to
-      // display only incomplete items
-      cy.contains("Active").click();
-
-      // After filtering, we can assert that there is only the one
-      // incomplete item in the list.
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Walk the dog");
-
-      // For good measure, let's also assert that the task we checked off
-      // does not exist on the page.
-      cy.contains("Pay electric bill").should("not.exist");
-    });
-
-    xit("can filter for completed tasks", () => {
-      // We can perform similar steps as the test above to ensure
-      // that only completed tasks are shown
-      cy.contains("Completed").click();
-
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .first()
-        .should("have.text", "Pay electric bill");
-
-      cy.contains("Walk the dog").should("not.exist");
-    });
-
-    xit("can delete all completed tasks", () => {
-      // First, let's click the "Clear completed" button
-      // `contains` is actually serving two purposes here.
-      // First, it's ensuring that the button exists within the dom.
-      // This button only appears when at least one task is checked
-      // so this command is implicitly verifying that it does exist.
-      // Second, it selects the button so we can click it.
-      cy.contains("Clear completed").click();
-
-      // Then we can make sure that there is only one element
-      // in the list and our element does not exist
-      cy.get(".todo-list li")
-        .should("have.length", 1)
-        .should("not.have.text", "Pay electric bill");
-
-      // Finally, make sure that the clear button no longer exists.
-      cy.contains("Clear completed").should("not.exist");
     });
   });
 });
