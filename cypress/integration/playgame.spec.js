c5d64b3161d075f4a5b94e470f456e459c34f246
sergnio <sergnioa@gmail.com>
2021-12-25 14:28:17 -0600

    remove node modules

--- cypress/integration/playgame.spec.js
+++ cypress/integration/playgame.spec.js
@@ -13,6 +13,53 @@
 
 const filePath = "cypress/fixtures/shouldContinueFindingRoutes.txt";
 
+/**
+ *
+ * @param {number} planeIndex
+ * @param {array} planeList
+ * @param {number} routeIndex
+ * @param {array} routeList
+ */
+const findRoute = (planeIndex, planeList, routeIndex, routeList) => {
+  cy.wrap(planeList[planeIndex])
+    .contains(": 0%")
+    .click()
+    .then(() => {
+      // base case - if we're at the end, break out
+      if (planeIndex >= planeList.length) {
+        cy.log("exiting");
+        cy.exec("exit", { timeout: 1 });
+      }
+
+      cy.log("actual route", routeList[routeIndex]);
+      cy.get(routeList[routeIndex]).click();
+
+      const routePAX = "tr#demandDay0 td.greenBonus";
+      cy.get(routePAX)
+        .first()
+        .invoke("text", (_, theText) => {
+          // text is formatted like "1190 PAX"
+          const paxValue = theText.split(" ")[0];
+
+          if (paxValue >= 1195) {
+            // selects the routes for all days and saves it
+            cy.get("td#caseIndex0").click();
+            cy.log("Great success", paxValue);
+            cy.get("img.planningDuplicate").first().click();
+            cy.get("input#planningSubmit")
+              .click()
+              .then(() => {
+                cy.log("clicked submit.. recursing");
+                cy.wait(4000);
+                findRoute(planeIndex + 1, planeList, 0, routeList);
+              });
+          } else {
+            cy.log("not enough pax.. not recursing", paxValue);
+          }
+        });
+    });
+};
+
 describe("example to-do app", () => {
   const automate = () => {
     cy.visit("https://tycoon.airlines-manager.com/network/planning");
@@ -24,48 +71,19 @@ describe("example to-do app", () => {
     cy.wait(500);
 
     // Look in the box that has all aircrafts
-    cy.get(".aircraftsBox").each(($el, index, $list) => {
+    cy.get(".aircraftsBox").each((aircraftBox, index, $list) => {
       // within that, find the "boxes" which have 6 aircrafts
-
       // loop through each of the planes (6 times)
-      cy.get("div.aircraftListMiniBox").each(($plane) => {
-        cy.wrap($plane).contains(": 0%").click();
-
-        // now find the routes which have 1195 passengers
-        // ** iterate over the routes **
-        // loop thru each route
-        cy.get("span.lineList").each(($line) => {
-          // cy.log("use1195 value:", found1195);
-          // if we've found it, just break it out
-          cy.wrap($line).click();
-
-          const routePAX = "tr#demandDay0 td.greenBonus";
-          cy.get(routePAX)
-            .first()
-            .invoke("text", (_, theText) => {
-              // text is formatted like "1190 PAX"
-              const paxValue = theText.split(" ")[0];
-
-              if (paxValue >= 1195) {
-                // selects the routes for all days and saves it
-                cy.get("td#caseIndex0").click();
-                cy.log("Great success", paxValue);
-                cy.get("img.planningDuplicate").first().click();
-                cy.get("input#planningSubmit")
-                  .click()
-                  .then(() => {
-                    cy.log("exiting");
-                    cy.exec("exit", { timeout: 1 });
-                  });
-                return false;
-              } else {
-                cy.log("not enough pax", paxValue).then(() => {
-                  cy.log("setting this to false");
-                });
-              }
-            });
-        });
-      });
+      cy.get("div.aircraftListMiniBox").each(
+        ($plane, planeIndex, planeList) => {
+          // now find the routes which have 1195 passengers
+          // ** iterate over the routes **
+          // loop thru each route
+          cy.get("span.lineList").each(($route, routeIndex, routeList) => {
+            findRoute(planeIndex, planeList, routeIndex, routeList, $route);
+          });
+        }
+      );
     });
   };
 
